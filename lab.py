35e0aadffbddfbdf33ccd2246cfc407c67ce2318
Firdoost <114271998+Firdoost@users.noreply.github.com>
2026-09-24 13:22:17 -0500

    Update lab.py

--- lab.py
+++ lab.py
@@ -6,3 +6,14 @@ print("Activity 1")
 pounds=int(input("What is the force in pounds?")
 newtons=(pounds*4.44822)
 print(pounds+" pounds force in Newtons is "+newtons+" newtons.")
+
+
+# Activity 2
+time_1 = input(int("Enter time one"))
+x_1 = input(int("Enter the first x position"))
+y_1 = input(int("Enter the first y position"))
+z_1 = input(int("Enter the first z position"))
+time_2 = input(int("Enter time two"))
+x_2 = input(int("Enter the second x position"))
+y_2 = input(int("Enter the second y position"))
+z_2 = input(int("Enter the second z position"))
