db3e99a37f519ac36e4d5c1f4eca9c3407c4618b
Firdoost <114271998+Firdoost@users.noreply.github.com>
2026-09-24 14:17:28 -0500

    Add comments explaining interpolation in 3D vectors

--- lab.py
+++ lab.py
@@ -21,3 +21,5 @@ z_2 = float(input("Enter the second z position: "))
 # V_interp = (1 - t) * A + t * B
 # I think t should be time in the formula becasue that part of the formula determines the position on the interpolated vectors. ex: t=1 is 100% of the vector, and t=0.5 is 50% of the vector.
 # It seems like there is supposed to be V_interp calculations for each component of the two positions: x, y, and z.
+# A is your starting 3D vector (x_1, y_1, z_1)
+# B is your ending 3D vector (x_2, y_2, z_2)
