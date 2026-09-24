ad2941f2a89253a92e4e7b833f21ccfb7b9e3eb9
alyssahinkel0507 <alyssahinkel0507@tamu.edu>
2026-09-24 13:18:53 -0500

    Update lab.py

--- lab.py
+++ lab.py
@@ -2,3 +2,7 @@ print("Howdy Team!")
 print("Team Lab 3")
 print("Team members: Alyssa, Brisania, Gabe, Vincent")
 print("Activity 1")
+#A
+pounds=int(input("What is the force in pounds?")
+newtons=(pounds*4.44822)
+print(pounds+" pounds force in Newtons is "+newtons+" newtons.")
