9887109e298b9341334d5d75c711a3d22e3a45ca
Firdoost <114271998+Firdoost@users.noreply.github.com>
2026-09-24 15:00:45 -0500

    Update lab.py

--- lab.py
+++ lab.py
@@ -18,6 +18,12 @@ x_2 = float(input("Enter the second x position: "))
 y_2 = float(input("Enter the second y position: "))
 z_2 = float(input("Enter the second z position: "))
 
+delta_time = time_5 - time_1
+time_sec = delta_time/4
+time_2 = time_sec + time_1
+time_3 = time_sec*2 + time_1
+time_4 = time_sec*3 + time_1
+
 # V_interp = (1 - t) * A + t * B
 # I think t should be time in the formula becasue that part of the formula determines the position on the interpolated vectors. ex: t=1 is 100% of the vector, and t=0.5 is 50% of the vector.
 # It seems like there is supposed to be V_interp calculations for each component of the two positions: x, y, and z.
