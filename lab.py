e9fcec2c2acd89f91c8baa46fbd91535747f5db3
Brisaniaandrade <156109971+Brisaniaandrade@users.noreply.github.com>
2026-09-24 18:08:03 -0500

    Refactor time calculations and output format

--- lab.py
+++ lab.py
@@ -8,32 +8,29 @@ newtons=(pounds*4.44822)
 print(pounds+" pounds force in Newtons is "+newtons+" newtons.")
 
 
-# Activity 2
+print("Activity 2")
 time_1 = float(input("Enter time one: "))
 x_1 = float(input("Enter the first x position: "))
 y_1 = float(input("Enter the first y position: "))
 z_1 = float(input("Enter the first z position: "))
-time_5 = float(input("Enter time two: "))
+time_2 = float(input("Enter time two: "))
 x_2 = float(input("Enter the second x position: "))
 y_2 = float(input("Enter the second y position: "))
 z_2 = float(input("Enter the second z position: "))
 
-delta_time = time_5 - time_1
-time_sec = delta_time/4
-time_2 = time_sec + time_1
-time_3 = time_sec*2 + time_1
-time_4 = time_sec*3 + time_1
-new_time = time_1
+delta_time = time_2 - time_1
+time_sec = delta_time/4   
+new_time = time_1   
 
-print(f"{'X':>10}{'Y':>10}{'Z':>10}{'Time':>10}")
+print(f"{'Time':>10}{'X':>10}{'Y':>10}{'Z':>10}")
 print("-" * 40)
 
 for t in [0, 0.25, 0.5, 0.75, 1]:
     v_int_x = (1 - t) * x_1 + t * x_2
     v_int_y = (1 - t) * y_1 + t * y_2
     v_int_z = (1 - t) * z_1 + t * z_2
-    print(f"{v_int_x:>10.2f}{v_int_y:>10.2f}{v_int_z:>10.2f}{new_time:>10.2f}")
-    new_time = new_time + time_sec
+    print(f"{new_time:>10.1f}{v_int_x:>10.2f}{v_int_y:>10.2f}{v_int_z:>10.2f}")
+    new_time = new_time + time_sec   # updates time for the next point  
 # V_interp = (1 - t) * A + t * B
 # v_1 = (1-t) * 
 
